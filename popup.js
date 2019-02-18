3d1b2b300e131923f5ba56accf784a8208ff952e
tngodup <tenzin.ngodup.rinchentsang@gmail.com>
2019-02-17 21:39:10 -0800

    open file

--- popup.js
+++ popup.js
@@ -16,7 +16,7 @@ chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
   }, function(selection) {
     // alert(selection[0]);
-    sendHighlightMessage();
+    sendHighlightMessage(selection[0]);
   });
 
 // $(function(){
--- selection.js
+++ selection.js
@@ -38,11 +38,36 @@ function highlightSelectedBlock () {
     // Add non disturbing border to selected elements
     // For simplicity I've adding outline only for the start element
     closestBlockElement.style.outline = '1px solid blue'
+
+    let selected_Text = window.getSelection().toString();
+
+    readTextFile("./index.html");
+
+    var reader = new FileReader();
+    content = reader.readAsText("./index.html");
+    console.log(content);
     
     // TODO Clear outline on some event: saving selection, ending selection etc
     // setTimeout(() => { closestBlockElement.style.outline = 'none' }, 2000)
   }
   
+  function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log("onmessage");
